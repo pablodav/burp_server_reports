87bab374f10e8441692dbb6fec07fa11299a88ab
pestigarribia <pablo.estigarribia@visitor.upm.com>
2016-06-24 18:09:50 -0300

    added some tests

--- burp_reports/backends/burpui_clients.py
+++ burp_reports/backends/burpui_clients.py
@@ -25,22 +25,34 @@ class Clients:
 
         self.backend = Burp(conf=self.buiconf)
 
-    def get_client(self, client_name=None):
+    def get_client(self, client):
         """
 
-        :param client_name: Name of the client to retrieve data.
-        :return: [{'received': 4025, 'end': 1466374965, 'encrypted': False, 'number': u'186', 'deletable': True, 'date': 1466374593, 'size': 2456396123}]
+        :param client: Name of the client to retrieve data.
+        :return: [{'received': 326753806, 'end': 1466714070, 'encrypted': False, 'number': 1, 'deletable': True, 'date': 1466713986, 'size': 572911431}]
         """
-        self.backend.get_client(name=client_name)
+        client_data = self.backend.get_client(name=client)
+        return client_data
 
     def get_clients(self):
         """
         #  server.get_all_clients()
         #
-        :return: [{'state': 'idle', 'last': 'never', 'name': u'clientname'}]
+        :return: [{'state': u'idle', 'last': 1466703186, 'name': u'monitor'}]
         """
         all_clients = self.backend.get_all_clients()
         return all_clients
+
+    def get_b_logs(self, number, client):
+        """
+
+        :param number: backup number
+        :param client: name of the client
+        :return:
+        """
+
+        backup_data = self.backend.get_backup_logs(self, number, client)
+        return backup_data
 
 
 
--- burp_reports/backends/test.py
+++ burp_reports/backends/test.py
@@ -1,3 +1,8 @@
+from burpui.misc.backend.burp2 import Burp
+from burp_reports.backends.burpui_clients import Clients
+clientes = Clients(burp_version=2, conf='/etc/burp/burp-ui.cfg')
 
+clientes.get_clients()
+clientes.get_client(client='monitor')
 
-class test()
+clientes.get_b_logs(client='monitor', number=1)

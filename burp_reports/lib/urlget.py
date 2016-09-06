3451df3df63c7989cdaa0c92c45c70c6b8722c91
pestigarribia <pablo.estigarribia@visitor.upm.com>
2016-09-06 12:18:25 -0300

    added retry 3 times to urlget lib

--- burp_reports/lib/urlget.py
+++ burp_reports/lib/urlget.py
@@ -6,6 +6,8 @@ import socket
 import requests_cache
 from datetime import timedelta
 from . files import temp_file
+# http://stackoverflow.com/questions/27062099/python-requests-retrying-until-a-valid-response-is-received
+import time
 
 cache_file = 'burp_reports_cache'
 expire_after = timedelta(hours=1)
@@ -28,7 +30,24 @@ def get_url_data(serviceurl, params=None):
 
     # Get data from the url
     # Support https without verification of certificate
-    req = requests.get(serviceurl, verify=False, params=params)
+    # req = requests.get(serviceurl, verify=False, params=params)
+
+    cnt = 0
+    max_retry = 3
+    while cnt < max_retry:
+        try:
+            req = requests.get(serviceurl, verify=False, params=params)
+            if req.json():
+                return req.json()
+            else:
+                # Raise a custom exception
+                raise ValueError('No data from response')
+
+        except requests.exceptions.RequestException as e:
+            time.sleep(2 ** cnt)
+            cnt += 1
+            if cnt >= max_retry:
+                raise e
 
     data = req.json()
 

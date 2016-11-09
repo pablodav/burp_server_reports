6b769b608706ecc0f7b3f1754dce9928c3d2179d
pestigarribia <pablo.estigarribia@visitor.upm.com>
2016-11-09 17:26:10 -0300

    added deco functions for parallel ping

--- setup.py
+++ setup.py
@@ -80,7 +80,8 @@ setup(name='burp_reports',
           'requests-cache',
           'pyzmail',
           'validators',
-          'rstcheck'
+          'rstcheck',
+          'deco'
       ],
       tests_require=['pytest'],
       zip_safe=False)

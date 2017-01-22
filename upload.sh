867ed39f13d4af0e7e20163940e289e0f93fe41d
Pablo Estigarribia <pablodav@gmail.com>
2017-01-22 12:28:15 -0300

    fix wrong name in upload

--- upload.sh
+++ upload.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # requires pip3 install wheel
 # for future simplicity evaluate flit
-sudo python3 -m pip install whell twine
+sudo python3 -m pip install wheel twine
 rm -r dist/*
 python3 setup.py sdist | grep defaults
 python3 setup.py bdist_wheel | grep defaults

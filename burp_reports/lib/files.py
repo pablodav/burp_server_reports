a8814276199e87c78bfa007622a3bb6c45b01e2f
Pablo Estigarribia <pablo.estigarribia@visitor.upm.com>
2017-05-17 13:31:22 -0300

    fix to use getpass.getuser() instead of os.getlogin()

--- burp_reports/lib/files.py
+++ burp_reports/lib/files.py
@@ -1,5 +1,6 @@
 import tempfile
 import os
+import getpass
 
 
 def temp_file(file='temporal'):
@@ -7,7 +8,7 @@ def temp_file(file='temporal'):
     return: str with tempfilename
     """
     # Append uid to end of filename
-    file += '_{}'.format(os.getlogin())
+    file += '_{}'.format(getpass.getuser())
     # Simplified and reutilized core funtionally from python
     cache_path = os.path.join(tempfile.gettempdir(), file)
 

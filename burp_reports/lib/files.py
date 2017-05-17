b76e91c4517e52528f8543fce276ff4b5af9a4f6
Pablo Estigarribia <pablo.estigarribia@visitor.upm.com>
2017-05-17 10:35:16 -0300

    fix temp file creation to something more multiplatform friendly

--- burp_reports/lib/files.py
+++ burp_reports/lib/files.py
@@ -7,7 +7,7 @@ def temp_file(file='temporal'):
     return: str with tempfilename
     """
     # Append uid to end of filename
-    file += '_{}'.format(os.getuid())
+    file += '_{}'.format(os.getlogin())
     # Simplified and reutilized core funtionally from python
     cache_path = os.path.join(tempfile.gettempdir(), file)
 

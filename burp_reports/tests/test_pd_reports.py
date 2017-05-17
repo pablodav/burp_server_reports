03bf2db5a04de7cda3a355944e04c70366454560
Pablo Estigarribia <pablo.estigarribia@visitor.upm.com>
2017-05-17 13:11:54 -0300

    update burp-ui demo

--- burp_reports/tests/test_pd_reports.py
+++ burp_reports/tests/test_pd_reports.py
@@ -4,7 +4,7 @@ from ..reports.pd_reports import BuiPdReports
 class TestPdReportsDemo:
 
     def test_pd_obj(self):
-        buireport = BuiPdReports(apiurl='https://admin:admin@demo.ziirish.me/api/')
+        buireport = BuiPdReports(apiurl='https://admin:admin@demo.burp-ui.org/api/')
         pd_brief_clients = buireport.clients_report()
         return pd_brief_clients
         

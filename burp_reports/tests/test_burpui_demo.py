3206fed10bb5b2a3ba5eda91a75b177d60508f0b
pablodav <pablodav@gmail.com>
2017-05-23 14:19:01 -0300

    fixes #10 due to wrong check for standalone mode on burpui

--- burp_reports/tests/test_burpui_demo.py
+++ burp_reports/tests/test_burpui_demo.py
@@ -2,9 +2,11 @@
 
 from ..interfaces.burpui_api_interface import BUIClients
 
+burpui_demo='https://admin:admin@demo.burp-ui.org/api/'
+burpui_standalone='http://admin:admin@172.17.0.2:8080/api/'
 
 def test_bui_demo():
-    clients_obj = BUIClients(burpui_apiurl='https://admin:admin@demo.burp-ui.org/api/')
+    clients_obj = BUIClients(burpui_apiurl=burpui_demo)
     clients_dict = clients_obj.translate_clients_stats()
 
     assert isinstance(clients_dict, dict)
@@ -12,7 +14,7 @@ def test_bui_demo():
     return clients_dict
 
 def test_bui_demo_detail():
-    clients_obj = BUIClients(burpui_apiurl='https://admin:admin@demo.burp-ui.org/api/')
+    clients_obj = BUIClients(burpui_apiurl=burpui_demo)
     clients_dict = clients_obj.translate_clients_stats(detail=True)
 
     assert isinstance(clients_dict, dict)

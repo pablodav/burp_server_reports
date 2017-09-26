1df554fc6094bf0b6d13e8e0dcec5c0f5b4bed26
Pablo Estigarribia <pablodav@gmail.com>
2017-09-26 19:52:35 -0300

    updated version to updated to pypi a smaller piece of code

--- upload.sh
+++ upload.sh
@@ -3,6 +3,10 @@
 # for future simplicity evaluate flit
 sudo python3 -m pip install wheel twine
 rm -r dist/*
+rm -r .tox
+rm -r .cache
+rm -r __pycache__
+rm .coverage
 python3 setup.py sdist | grep defaults
 python3 setup.py bdist_wheel | grep defaults
 twine upload dist/*

a5248dfe2534713bcfa7007de85130967cb84d95
Pablo Estigarribia <pablodav@gmail.com>
2018-03-13 23:19:14 -0300

    modify tests and upgrade circleci tests

--- burp_reports/tests/test_burpui_demo.py
+++ burp_reports/tests/test_burpui_demo.py
@@ -1,6 +1,7 @@
 #!python3
 
 from ..interfaces.burpui_api_interface import BUIClients
+from ..backends.burpui_api import Clients
 
 burpui_demo='https://admin:admin@demo.burp-ui.org/api/'
 burpui_standalone='http://admin:admin@172.17.0.2:8080/api/'
@@ -20,3 +21,15 @@ def test_bui_demo_detail():
     assert isinstance(clients_dict, dict)
 
     return clients_dict
+
+def test_burpui_api_multi():
+    bui_api_obj = Clients(apiurl=burpui_demo)
+    clients = bui_api_obj._get_clients_report_multi()
+
+    assert isinstance(clients, list)
+
+def test_burpui_api_brief():
+    bui_api_obj = Clients(apiurl=burpui_demo)
+    clients = bui_api_obj.get_clients_reports_brief()
+
+    assert isinstance(clients, list)
